94520922479257733d979f9a3c0132cf243280a3
KuTaKe <1449568043@qq.com>
2023-10-29 00:27:27 +0800

    🦄 refactor: move source code

--- bump-version.py
+++ bump-version.py
@@ -3,7 +3,7 @@ import re
 import sys
 
 tag_name = sys.argv[1]
-init_file = pathlib.Path("./src/__init__.py")
+init_file = pathlib.Path("./src/pyclaapi/__init__.py")
 code = init_file.read_text(encoding="utf-8")
 code = re.sub(r"__version__ = \".+?\"", f'__version__ = "{tag_name}"', code)
 init_file.write_text(code, "utf-8")
